5439c683358e0e25426370805f192610f3cc8bfd
muneebGH <muneeb.github@gmail.com>
2020-03-26 15:06:03 +0500

    improved ui,shows different cell for other

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -57,6 +57,8 @@ class ChatViewController: UIViewController {
                     }
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
+                        let indexPath=IndexPath(row: self.messages.count-1, section: 0)
+                        self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                     }
                     
                 }
@@ -75,6 +77,9 @@ class ChatViewController: UIViewController {
                     print("error in saving to firestore")
                 }else{
                     print("successfuly saved message to firestore")
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text=""
+                    }
                     
                 }
             }
@@ -104,8 +109,20 @@ extension ChatViewController:UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message=messages[indexPath.row]
         let cell=tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath) as! MessageCell
-        cell.label.text=messages[indexPath.row].body
+        cell.label.text=message.body
+        if message.sender == Auth.auth().currentUser?.email{
+            cell.leftImageView.isHidden=true
+            cell.rightImageView.isHidden=false
+            cell.messageBubble.backgroundColor=UIColor(named: Constants.BrandColors.lightPurple)
+            cell.label.textColor=UIColor(named:Constants.BrandColors.purple)
+        }else{
+            cell.leftImageView.isHidden=false
+            cell.rightImageView.isHidden=true
+            cell.messageBubble.backgroundColor=UIColor(named: Constants.BrandColors.purple)
+            cell.label.textColor=UIColor(named:Constants.BrandColors.lightPurple)
+        }
         return cell
     }
     
